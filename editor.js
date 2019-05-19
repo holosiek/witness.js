90abb94402b9dd064944ca439d8ee75019883b10
Holo <holo@frozent.pl>
2019-05-19 02:46:18 +0200

    Level editor [part 2] - Finished

--- editor.js
+++ editor.js
@@ -1,140 +1,276 @@
-board = { 'sizeX' : 11, 'sizeY' : 11, 'block' : 0, 'grid' : [] }
+// Create main object
+Board = { 
+    'sizeX' : 11, 
+    'sizeY' : 11, 
+    'block' : 0, 
+    'grid' : [], 
+    'blocks' : [], 
+    'lines': { 'x': [],'y': [] } 
+}
+
+// Create board and set variables
 function boardCreate(){
-    board.grid = [];
-    document.getElementById("boar").style.width = (30*board.sizeY) + "px"
-    document.getElementById("boar").style.height = (30*board.sizeX) + "px"
+    // Reset all variables
+    Board.grid   = [];
+    Board.blocks = [];
+    Board.lines  = { 'x': [], 'y': [] };
+    
+    // Set width/height of board and reset it
+    document.getElementById("boar").style.width  = (50*Board.sizeX) + "px"
+    document.getElementById("boar").style.height = (50*Board.sizeY) + "px"
     document.getElementById("boar").innerHTML = ""
-    for(var i=0; i<board.sizeX; i++){
-        for(var j=0; j<board.sizeY; j++){
-            let xd = document.createElement("div");
-            if( (i%2) == 0 && i != 0 && i != board.sizeX-1 && (j%2) == 0 && j != 0 && j != board.sizeY-1 ){
-                xd.className = "cube-box";
-                xd.innerHTML = "b_" + i + "_" + j;
-                xd.id = "b_" + i + "_" + j;
-                xd.addEventListener('click',()=>{
-                    clicked(xd)
-                });
+    
+    // Create grid on the board
+    for(var y=0; y < Board.sizeY; y++){
+        for(var x=0; x < Board.sizeX; x++){
+            let elem = document.createElement("div");
+            if( x != 0 && x != Board.sizeX-1 && y != 0 && y != Board.sizeY-1 && x%2 == 0 && y%2 == 0 ){
+                elem.className = "box cube-box";
+                elem.id = "c_" + x + "_" + y;
+                elem.addEventListener('click', () => { cubeClicked(elem); });
+            } else if( (y-x%2)%2 == 1 && x != 0 && x != Board.sizeX-1 && y != 0 && y != Board.sizeY-1 ){
+                elem.className = "box line-box";
+                elem.id = "l_" + x + "_" + y;
+                elem.addEventListener('click', () => { lineClicked(elem); });
             } else {
-                xd.className = "box";
-                xd.innerHTML = "b_" + i + "_" + j;
-                xd.id = "b_" + i + "_" + j;
-                xd.addEventListener('click',()=>{
-                    clicked(xd)
-                });
+                elem.className = "box";
+                elem.id = "b_" + x + "_" + y;
+                elem.addEventListener('click', () => { gridClicked(elem); });
             }
-            
-            document.getElementById("boar").appendChild(xd)
+            document.getElementById("boar").appendChild(elem)
         }
     }
-    for(var i=0; i<board.sizeY; i++){
-        var gride = [];
-            gride.length = board.sizeX;
+    
+    // Reset and create new grid variable to store which blocks are placed
+    for(let i = 0; i < Board.sizeY; i++){
+        let gride = [];
+            gride.length = Board.sizeX;
             gride.fill({ "type": "none" })
-        board.grid.push(gride)
+        Board.grid.push(gride)
+    }
+    
+    // Fill lines array
+    let calculateArrayXList = Math.floor((Board.sizeX-2)/2);
+    for(let i = 0; i < calculateArrayXList; i++){
+        let gride = [];
+            gride.length = (Board.sizeX-1)/2;
+            gride.fill(0);
+        Board.lines.x.push(gride);
+    }
+    let calculateArrayYList = Math.floor((Board.sizeY-2)/2);
+    for(let i = 0; i < calculateArrayYList; i++){
+        let gride = [];
+            gride.length = (Board.sizeY-1)/2;
+            gride.fill(0);
+        Board.lines.y.push(gride);
     }
 }
-boardCreate()
-function changeColor(x){
-    board.block = x;
-    for(var i=0; i<document.getElementsByClassName("menu-button").length; i++){
-        if(i == x){
-            document.getElementsByClassName("menu-button")[i].className = "menu-button menu-button-on"
-        } else {
-            document.getElementsByClassName("menu-button")[i].className = "menu-button"
-        }
-    }
-}
-function clicked(x){
-    var whatsId = x.id.split('_')
-    switch(board.block){
+
+/*
+    Click events for grid
+    0 - start
+    1 - line
+    2 - connector
+    3/4/5/6 - end
+    7 - noend
+    8/9 - nothing
+*/
+function gridClicked(elem){
+    var whatsId = elem.id.split('_')
+    switch(Board.block){
         case 0:
-            x.style.backgroundImage = "url('img/start.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "start" };
-            break;
-        case 1:
-            x.style.backgroundImage = "url('img/hor_line.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "line" };
+            //elem.style.backgroundImage = "url('img/start.png')";
+            elem.innerHTML = "Start"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "start" };
             break;
         case 2:
-            x.style.backgroundImage = "url('img/ver_line.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "line" };
+            //elem.style.backgroundImage = "url('img/connector.png')";
+            elem.innerHTML = "Conn"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "connector" };
             break;
         case 3:
-            x.style.backgroundImage = "url('img/connector.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "connector" };
+            //elem.style.backgroundImage = "url('img/endbottom.png')";
+            elem.innerHTML = "End ▼"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "endbottom" };
             break;
         case 4:
-            x.style.backgroundImage = "url('img/endbottom.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "endbottom" };
+            //elem.style.backgroundImage = "url('img/endtop.png')";
+            elem.innerHTML = "End ▲"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "endtop" };
             break;
         case 5:
-            x.style.backgroundImage = "url('img/endtop.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "endtop" };
+            //elem.style.backgroundImage = "url('img/endleft.png')";
+            elem.innerHTML = "End ◀"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "endleft" };
             break;
         case 6:
-            x.style.backgroundImage = "url('img/endleft.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "endleft" };
+            //elem.style.backgroundImage = "url('img/endright.png')";
+            elem.innerHTML = "End ▶"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "endleft" };
             break;
         case 7:
-            x.style.backgroundImage = "url('img/endright.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "endleft" };
+            //elem.style.backgroundImage = "url('img/noend.png')";
+            elem.innerHTML = "⛌"
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "noend" };
             break;
         case 8:
-            x.style.backgroundImage = "url('img/noend.png')";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "noend" };
+            //elem.style.backgroundImage = "";
+            elem.innerHTML = "";
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "none" };
+            break;
+    }
+}
+
+function lineClicked(elem){
+    var whatsId = elem.id.split('_')
+    switch(Board.block){
+        case 1:
+            //elem.style.backgroundImage = "url('img/hor_line.png')";
+            elem.innerHTML = "Path";
+            if(whatsId[1]%2 == 0){
+                Board.lines.x[(whatsId[1]/2)-1][(whatsId[2]-1)/2] = 1;
+            } else {
+                Board.lines.y[(whatsId[2]/2)-1][(whatsId[1]-1)/2] = 1;
+            }
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "line" };
             break;
         case 9:
-            x.style.backgroundImage = "";
-            board.grid[whatsId[2]][whatsId[1]] = { "type": "none" };
+            //elem.style.backgroundImage = "";
+            elem.innerHTML = "";
+            if(whatsId[1]%2 == 0){
+                Board.lines.x[(whatsId[1]/2)-1][(whatsId[2]-1)/2] = 0;
+            } else {
+                Board.lines.y[(whatsId[2]/2)-1][(whatsId[1]-1)/2] = 0;
+            }
+            Board.grid[whatsId[1]][whatsId[2]] = { "type": "none" };
             break;
     }
 }
-function restore(x,y){
-    switch(y.type){
+
+function cubeClicked(elem){
+    
+}
+
+/*
+    #####################################################
+    SAVE AND LOAD MAP
+*/
+// Load map functions
+function gridRestore(id,block){
+    elem = document.getElementById(id);
+    switch(block.type){
         case "start":
-            document.getElementById(x).style.backgroundImage = "url('img/start.png')";
-            break;
-        case "line":
-            document.getElementById(x).style.backgroundImage = "url('img/hor_line.png')";
+            //elem.style.backgroundImage = "url('img/start.png')";
+            elem.innerHTML = "Start"
             break;
         case "connector":
-            document.getElementById(x).style.backgroundImage = "url('img/connector.png')";
+            //elem.style.backgroundImage = "url('img/connector.png')";
+            elem.innerHTML = "Conn"
             break;
         case "endbottom":
-            document.getElementById(x).style.backgroundImage = "url('img/endbottom.png')";
+            //elem.style.backgroundImage = "url('img/endbottom.png')";
+            elem.innerHTML = "End ▼"
             break;
         case "endtop":
-            document.getElementById(x).style.backgroundImage = "url('img/endtop.png')";
+            //elem.style.backgroundImage = "url('img/endtop.png')";
+            elem.innerHTML = "End ▲"
             break;
         case "endleft":
-            document.getElementById(x).style.backgroundImage = "url('img/endleft.png')";
+            //elem.style.backgroundImage = "url('img/endleft.png')";
+            elem.innerHTML = "End ◀"
             break;
-        case "endleft":
-            document.getElementById(x).style.backgroundImage = "url('img/endright.png')";
+        case "endright":
+            //elem.style.backgroundImage = "url('img/endright.png')";
+            elem.innerHTML = "End ▶"
             break;
         case "noend":
-            document.getElementById(x).style.backgroundImage = "url('img/noend.png')";
+            //elem.style.backgroundImage = "url('img/noend.png')";
+            elem.innerHTML = "⛌"
             break;
         case "none":
-            document.getElementById(x).style.backgroundImage = "";
+            //elem.style.backgroundImage = "";
+            elem.innerHTML = "";
             break;
     }
 }
-changeColor(board.block);
-function savePuzzle(){
-    window.prompt("Copy it to clipboard and save it to file",JSON.stringify(board.grid))
+
+function lineRestore(id,block){
+    elem = document.getElementById(id);
+    switch(block.type){
+        case "line":
+            //elem.style.backgroundImage = "url('img/hor_line.png')";
+            elem.innerHTML = "Path";
+            break;
+        case "none":
+            //elem.style.backgroundImage = "";
+            elem.innerHTML = "";
+            break;
+    }
 }
+
+function cubeRestore(id,block){
+    elem = document.getElementById(id);
+    switch(block.type){
+        
+    }
+}
+
+// Load puzzle
 function loadPuzzle(){
+    // Ask for code
     saved = JSON.parse(window.prompt("Paste puzzle code"));
-    board.sizeX = saved.length;
-    board.sizeY = saved[0].length;
-    document.getElementById("inputGridX").value = board.sizeX;
-    document.getElementById("inputGridY").value = board.sizeY;
+    // Set board size
+    Board.sizeX = saved.objects.length;
+    Board.sizeY = saved.objects[0].length;
+    // Create board from new size
     boardCreate();
-    board.grid = saved;
-    for(var i=0; i<board.sizeX; i++){
-        for(var j=0; j<board.sizeY; j++){
-            restore("b_"+i+"_"+j,board.grid[j][i])
+    // Set variables
+    Board.grid   = saved.objects;
+    Board.blocks = saved.blocks;
+    Board.lines  = saved.lines;
+    // Set new values of inputs
+    document.getElementById("inputGridX").value = Board.sizeX;
+    document.getElementById("inputGridY").value = Board.sizeY;
+    // Restore board data
+    for(var y = 0; y < Board.sizeY; y++){
+        for(var x = 0; x < Board.sizeX; x++){
+            if( x != 0 && x != Board.sizeX-1 && y != 0 && y != Board.sizeY-1 && x%2 == 0 && y%2 == 0 ){
+                cubeRestore("c_"+x+"_"+y, Board.grid[x][y]);
+            } else if( (y-x%2)%2 == 1 && x != 0 && x != Board.sizeX-1 && y != 0 && y != Board.sizeY-1 ){
+                lineRestore("l_"+x+"_"+y, Board.grid[x][y]);
+            } else {
+                gridRestore("b_"+x+"_"+y, Board.grid[x][y]);
+            }
         }
     }
 }
+
+// SAVE PUZZLE
+function savePuzzle(){
+    let save = {}
+        save["objects"] = Board.grid;
+        save["lines"] = Board.lines;
+        save["blocks"] = Board.blocks;
+    window.prompt("Copy this to clipboard and paste to the game:", JSON.stringify(save))
+}
+
+
+/*
+    #####################################################
+    MISC. FUNCTIONS
+*/
+// Change color of the button
+function changeColor(x){
+    Board.block = x;
+    for(var i=0; i<document.getElementsByClassName("menu-button").length; i++){
+        document.getElementsByClassName("menu-button")[i].className = "menu-button"
+    }
+    document.getElementById("tool_"+x).className = "menu-button menu-button-on"
+}
+
+// Initalize whole editor
+function Init(){
+    boardCreate();
+    changeColor(Board.block);
+}
+Init();
